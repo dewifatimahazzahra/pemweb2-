3715fd6a4028b819b731f812d2dfb2aef11a7a09
cah.bungas <147271408+dewifatimahazzahra@users.noreply.github.com>
2024-04-19 09:52:07 +0700

    Add files via upload

---
+++ praktek4/admin/daftar.php
@@ -0,0 +1,13 @@
+<?php 
+    include_once "top.php";
+    include_once "menu.php";
+?>
+<div class="container-fluid px-4">
+                        <h1 class="mt-4">Silahkan isi form pendaftaran</h1>
+                        
+                        <div style="height: 100vh"></div>
+                        <div class="card mb-4"><div class="card-body">When scrolling, the navigation stays at the top of the page. This is the end of the static navigation demo.</div></div>
+                    </div>
+<?php 
+    include_once "bottom.php";
+?>
---
+++ praktek4/adminsimpsidebar/produk.php
@@ -0,0 +1,17 @@
+<?php 
+include_once "layout/top.php";
+include_once "layout/menu.php";
+?>
+
+<!-- Page content-->
+                <div class="container-fluid">
+                    <h1 class="mt-4">SELAMAT DATANG DI HALAMAN PRODUK</h1>
+                    
+                    </p>
+                </div>
+            </div>
+        </div>
+
+<?php 
+include_once "layout/bottom.php";
+?>
